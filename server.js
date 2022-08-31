23f41ec10d8114a482ec9955a056412be7b20a0e
ARJUN <arjunsharman06@gmail.com>
2022-08-31 16:39:28 -0400

    added the mongoDb connection and server connection

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+// Connection to mongoDB
+mongoose.connect(
+  process.env.MONODB_URI || `mongodb://127.0.0.1:27017/${process.env.DB_NAME}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`Connected to the Port = ${PORT}`));
